58dd64c4eb120a02aa7028f9b9e6ca4d6d8e2c7e
William Ravel <wravel@gmail.com>
2018-07-24 00:36:42 +0200

    Update

--- a.lisp
+++ a.lisp
@@ -229,7 +229,7 @@
 (defun make-edge-list ()
   (apply #'append (loop repeat *edge-num*
                      collect (edge-pair (random-node) (random-node)))))
-;; p con189
+;; p 189
 (defun direct-edges (node edge-list)
   (remove-if-not (lambda (x)
                    (eql (car x) node))
@@ -242,7 +242,9 @@
                  (push node visited)
                  (mapc (lambda (edge)
                          (traverse (cdr edge)))
-                       (direct-edges node edge-list))))))))
+                       (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
 
 (defun find-islands (nodes edge-list)
   (let ((islands nil))
@@ -263,3 +265,56 @@
 (defun connect-all-islands (nodes edge-list)
   (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
 
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num*
+                   collect i))
+         (edge-list (connect-all-islands nodes (make-edge-list)))
+         (cops (remove-if-not (lambda (x)
+                                (zerop (random *cop-odds*)))
+                              edge-list)))
+    (add-cops (edges-to-alist edge-list) cops)))
+
+(defun edge-to-alist (edge-list)
+  (mapcar (lambda (node1)
+            (cons node1
+                  (mapcar (lambda (edge)
+                            (list (cdr edge)))
+                          (remove-duplicates (direct-edges node1 edge-list)
+                                             :test #'equal))))
+          (remove-duplicates (mapcar #'car edge-list))))
+
+(defun add-cops (edge-alist edges-with-cops)
+  (mapcar (lambda (x)
+            (let ((node1 (car x))
+                  (node1-edges (cdr x)))
+              (cons node1
+                    (mapcar (lambda (edge)
+                              (let ((node2 (car edge)))
+                                (if (intersection (edge-pair node1 node2) edges-with-cops
+                                                  :test #'equal)
+                                    (list node2 'cops)
+                                    edge)))
+                            node1-edges))))
+          edge-alist))
+
+(defun neighbors (node edge-alist)
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+(defun within-one (a b edge-alist)
+  (member b (neighbors a edge-alist)))
+
+(defun whithin-two (a b edge-alist)
+  (or (within-one a b edge-alist)
+      (some (lambda (x)
+              (within-one x b edge-alist))
+            (neighbors a edge-alist))))
+
+(defun make-city-nodes (edge-alist)
+  (let ((wumpus (random-node))
+        (glow-worms (loop for i below *worm-num*
+                       collect (random-node))))
+    (loop for n from 1 to *node-num*
+       collect (append (list n)
+                       (cond ((eql n wumpus) '(wumpus)))))))
+
+; p199

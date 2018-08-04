974b9aa846eff25430158ad4da9d5aeed63e01b4
William Ravel <wravel@gmail.com>
2018-08-04 21:05:02 +0200

    Update

--- thread.lisp
+++ thread.lisp
@@ -62,3 +62,66 @@
 (defmethod withdraw ((account bank-account) (amount real))
   (decf (:balance account) amount))
 
+
+(defparameter *rich*
+  (make-instance 'bank-account
+                 :id 1
+                 :name "Rich"
+                 :balance 0))
+
+(defun demo-race-condition ()
+  (loop repeat 100
+     do
+       (bt:make-thread
+        (lambda ()
+          (loop repeat 10000 do (deposit *rich* 100))
+          (loop repeat 10000 do (withdraw *rich* 100))))))
+
+(defvar *lock* (bt:make-lock))
+
+(defun demo-race-condition-locks ()
+  (loop repeat 100
+     do
+       (bt:make-thread
+        (lambda ()
+          (loop repeat 10000 do (bt:with-lock-held (*lock*)
+                                  (deposit *rich* 100)))
+          (loop repeat 10000 do (bt:with-lock-held (*lock*)
+                                  (withdraw *rich* 100)))))))
+
+
+(defmacro until (condition &body body)
+  (let ((block-name (gensym)))
+    `(block ,block-name
+       (loop
+          (if ,condition
+              (return-from ,block-name nil)
+              (progn
+                ,@body))))))
+
+(defun join-destroy-thread ()
+  (let* ((s *standard-output*)
+         (joiner-thread (bt:make-thread
+                         (lambda ()
+                           (loop for i from 1 to 10
+                              do
+                                (format s "~%[Joiner Thread]  Working...")
+                                (sleep (* 0.01 (random 100)))))))
+         (destroyer-thread (bt:make-thread
+                            (lambda ()
+                              (loop for i from 1 to 1000000
+                                 do
+                                   (format s "~%[Destroyer Thread] Working...")
+                                   (sleep (* 0.01 (random 10000))))))))
+    (format t "~%[Main Thread] Waiting on joiner thread...")
+    (bt:join-thread joiner-thread)
+    (format t "~%[Main Thread] Done waiting on joiner thread")
+    (if (bt:thread-alive-p destroyer-thread)
+        (progn
+          (format t "~%[Main Thread] Destroyer thread alive... killing it")
+          (bt:destroy-thread destroyer-thread))
+        (format t "~%[Main Thread] Destroyer thread is already dead"))
+    (until (bt:thread-alive-p destroyer-thread)
+           (format t "[Main Thread] Waiting for destroyer thread to die..."))
+    (format t "~%[Main Thread] Destroyer thread dead")
+    (format t "~%[Main Thread] Adios!~%")))
